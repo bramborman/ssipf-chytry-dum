cb8907a5dba3ec3731e5cbc9ae6949a688b7906d
bramborman <mariandolinsky@outlook.com>
2019-06-14 14:39:32 +0200

    Zpřehlednění a optimalizace programu pro lampy v ulici

--- src/ulice_slave_lampy/ulice_slave_lampy.ino
+++ src/ulice_slave_lampy/ulice_slave_lampy.ino
@@ -1,166 +1,109 @@
+#include <Wire.h>
 
-   #include <Wire.h>
+// Odkomentujte následující řádek
+// pro zapnutí vypisování testovacích
+// zpráv do sériového monitoru
+// #define SERIAL_DEBUG
 
-   int a = 0;
-   int karta = 0;
-   int c1 = 0;
-   
-    //lampa 1
-    int redPin1 = 13;
-    int greenPin1 = 11;
-    int bluePin1 = 12;
+#define LED_ARRAY_SIZE 3
+#define LED_ARRAY_RED 0
+#define LED_ARRAY_GREEN 1
+#define LED_ARRAY_BLUE 2
 
-    //lampa 2
-    int redPin2 = 51;
-    int greenPin2 = 53;
-    int bluePin2 = 52;
+// Definice pinů pomocí polí pro jednodušší práci
+// Piny MUSÍ být v pořadí RGB
+const int OUT_LED_1[LED_ARRAY_SIZE] = { 13, 11, 12 };
+const int OUT_LED_2[LED_ARRAY_SIZE] = { 51, 53, 52 };
+const int OUT_LED_3[LED_ARRAY_SIZE] = { 48, 49, 50 };
+const int OUT_LED_4[LED_ARRAY_SIZE] = {  9,  8, 10 };
 
-    //lampa 3
-    int redPin3 = 48;
-    int greenPin3 = 49;
-    int bluePin3 = 50;
+int a = 0;
+int karta = 0;
+int c1 = 0;
 
-    //lampa 4
-    int redPin4 = 9;
-    int greenPin4 = 8;
-    int bluePin4 = 10;
+void setup() {
+  // join i2c bus with address #10
+  Wire.begin(10);
+  Wire.onReceive(receiveEvent);
 
+#ifdef SERIAL_DEBUG
+  Serial.begin(9600);
+#endif
 
-void setup(){
-     
-     Wire.begin(10);                // join i2c bus with address #10
-     Wire.onReceive(receiveEvent);
-     Serial.begin(9600);
+  for (int i = 0; i < LED_ARRAY_SIZE; i++) {
+    pinMode(OUT_LED_1[i], OUTPUT);
+    pinMode(OUT_LED_2[i], OUTPUT);
+    pinMode(OUT_LED_3[i], OUTPUT);
+    pinMode(OUT_LED_4[i], OUTPUT);
+  }
 
-     pinMode(greenPin1, OUTPUT);
-     pinMode (redPin1, OUTPUT);
-     pinMode (bluePin1, OUTPUT);
+  lampa_vypnout(OUT_LED_1);
+  lampa_vypnout(OUT_LED_2);
+  lampa_vypnout(OUT_LED_3);
+  lampa_vypnout(OUT_LED_4);
 
-     pinMode(greenPin2, OUTPUT);
-     pinMode (redPin2, OUTPUT);
-     pinMode (bluePin2, OUTPUT);
-
-     pinMode(greenPin3, OUTPUT);
-     pinMode (redPin3, OUTPUT);
-     pinMode (bluePin3, OUTPUT);
-
-     pinMode(greenPin4, OUTPUT);
-     pinMode (redPin4, OUTPUT);
-     pinMode (bluePin4, OUTPUT);
-
-     digitalWrite (greenPin1,LOW);
-     digitalWrite (greenPin2,LOW);
-     digitalWrite (redPin1,LOW);
-     digitalWrite (redPin2,LOW); 
-     digitalWrite (bluePin1,LOW);
-     digitalWrite (bluePin2,LOW);
-     digitalWrite (greenPin3,LOW);
-     digitalWrite (greenPin4,LOW);
-     digitalWrite (redPin3,LOW);
-     digitalWrite (redPin4,LOW); 
-     digitalWrite (bluePin3,LOW);
-     digitalWrite (bluePin4,LOW);
-     zhasni12();
-     zhasni34();
-     c1=0;
-     karta = 0;
+  c1 = 0;
+  karta = 0;
 }
 
 void loop() {
-
- delay(10);
-  
- //karta = random(0,3) + 1; //vybere se barva
- 
+  delay(10);
   karta = a;
-      if (karta == c1){
-      
-      zhasni34(); //zhasne se lampa 3 a 4
-      
-      zhasni12(); //zhasne se lampa 1 a 2
-       }
-       else{
-          lampa34(karta); //rožne se lampa 3 a 4
-          delay (9000);
-          c1 = karta;
-          lampa12(c1); //rožne se lampa 1 a 2
-          delay (1000);
-          zhasni34(); //zhasne se lampa 3 a 4
-          delay (8500);
-          zhasni12(); //zhasne se lampa 1 a 2
-          delay (1000);
-         }
-  karta=0;
-}
 
-void lampa34(int b){
-  if (b == 1){
-   // Serial.println("lampa34 barva G");
-    digitalWrite (greenPin3,HIGH);
-    digitalWrite (greenPin4,HIGH);
-  }
-  else if (b == 2){
-    //Serial.println("lampa34 barva RB");
-    digitalWrite (redPin3,HIGH);
-    digitalWrite (redPin4,HIGH);
-    digitalWrite (greenPin3,HIGH);
-    digitalWrite (greenPin4,HIGH);
+  if (karta == c1) {
+    lampa_vypnout(OUT_LED_1);
+    lampa_vypnout(OUT_LED_2);
+    lampa_vypnout(OUT_LED_3);
+    lampa_vypnout(OUT_LED_4);
   }
   else {
-    if (b == 3){
-   // Serial.println("lampa34 barva GB");
-    digitalWrite (greenPin3,HIGH);
-    digitalWrite (greenPin4,HIGH);
-    digitalWrite (bluePin3,HIGH);
-    digitalWrite (bluePin4,HIGH);
-    }
-  } 
+    lampa_zapnout(OUT_LED_3, karta);
+    lampa_zapnout(OUT_LED_4, karta);
+
+    delay(9000);
+    c1 = karta;
+
+    lampa_zapnout(OUT_LED_1, c1);
+    lampa_zapnout(OUT_LED_2, c1);
+
+    delay(1000);
+    lampa_vypnout(OUT_LED_3);
+    lampa_vypnout(OUT_LED_4);
+
+    delay(8500);
+    lampa_vypnout(OUT_LED_1);
+    lampa_vypnout(OUT_LED_2);
+
+    delay(1000);
+  }
+
+  karta = 0;
 }
 
-void lampa12(int d){
-  if (d == 1){
-   // Serial.println("lampa12 barva G");
-     digitalWrite (greenPin1,HIGH);
-     digitalWrite (greenPin2,HIGH);
+void lampa_zapnout(const int led[LED_ARRAY_SIZE], const int b) {
+  if (b == 1) {
+    digitalWrite(led[LED_ARRAY_GREEN], HIGH);
   }
-  else if (d == 2){
-  // Serial.println("lampa12 barva RB");
-   digitalWrite (redPin1,HIGH);
-   digitalWrite (redPin2,HIGH);
-   digitalWrite (greenPin1,HIGH);
-   digitalWrite (greenPin2,HIGH);
+  else if (b == 2) {
+    digitalWrite(led[LED_ARRAY_RED], HIGH);
+    digitalWrite(led[LED_ARRAY_GREEN], HIGH);
   }
-  else {
-    if (d == 3){
-   // Serial.println("lampa12 barva GB");
-    digitalWrite (greenPin1,HIGH);
-    digitalWrite (greenPin2,HIGH);
-    digitalWrite (bluePin1,HIGH);
-    digitalWrite (bluePin2,HIGH);
-    }
+  else if (b == 3) {
+    digitalWrite(led[LED_ARRAY_GREEN], HIGH);
+    digitalWrite(led[LED_ARRAY_BLUE], HIGH);
   }
- }
-
-void zhasni34(){
-   digitalWrite (greenPin3,LOW);
-   digitalWrite (greenPin4,LOW);
-   digitalWrite (redPin3,LOW);
-   digitalWrite (redPin4,LOW); 
-   digitalWrite (bluePin3,LOW);
-   digitalWrite (bluePin4,LOW);     
- }
-
-void zhasni12(){
-  digitalWrite (greenPin1,LOW);
-  digitalWrite (greenPin2,LOW);
-  digitalWrite (redPin1,LOW);
-  digitalWrite (redPin2,LOW); 
-  digitalWrite (bluePin1,LOW);
-  digitalWrite (bluePin2,LOW);
 }
 
+void lampa_vypnout(const int led[LED_ARRAY_SIZE]) {
+  digitalWrite(led[LED_ARRAY_RED], LOW);
+  digitalWrite(led[LED_ARRAY_GREEN], LOW);
+  digitalWrite(led[LED_ARRAY_BLUE], LOW);
+}
 
 void receiveEvent(int howMany) {
-  a = Wire.read();    
-  Serial.println(a); 
- }
+  a = Wire.read();
+
+#ifdef SERIAL_DEBUG
+  Serial.println(a);
+#endif
+}
